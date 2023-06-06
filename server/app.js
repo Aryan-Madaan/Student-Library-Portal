dcd456d980ac436ee81462e4ea7f958d7e03d92c
Shardul Joshi <yo.shardul@gmail.com>
2023-06-06 16:37:56 +0530

    Added 2 .json files for testing & modified app.js file with 2 routes correspondingly

--- server/app.js
+++ server/app.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res, next) => {
-  res.send("Fix VK ka flush");
+const testStu = require("./testStudent.json");
+const testBook = require("./testBooks.json");
+
+app.get("/students", (req, res, next) => {
+  res.send(testStu);
+});
+
+app.get("/books", (req, res, next) => {
+  res.send(testBook);
 });
 
 app.listen(8080, () => {
